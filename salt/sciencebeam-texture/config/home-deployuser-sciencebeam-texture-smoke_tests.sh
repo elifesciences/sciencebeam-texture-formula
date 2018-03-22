c3ea4d6d1f9f7850c18da10e53080dca6f018bf4
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-22 15:23:46 +0000

    Smoke test GROBID

--- salt/sciencebeam-texture/config/home-deployuser-sciencebeam-texture-smoke_tests.sh
+++ salt/sciencebeam-texture/config/home-deployuser-sciencebeam-texture-smoke_tests.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 . /opt/smoke.sh/smoke.sh
 
+echo "Texture container"
+smoke_url_ok localhost:4000/
+echo "Sciencebeam container"
+smoke_url_ok localhost:8075/api/
+echo "GROBID container"
+smoke_url_ok localhost:8070/
+echo "Texture nginx"
 smoke_url_ok localhost/
-#smoke_url_ok localhost/sciencebeam
+echo "Sciencebeam nginx"
+smoke_url_ok localhost/sciencebeam/api/
 smoke_report
