de05168954fb10e6c8ba539d9d17f7e3eaadce8c
Daniel Ecer <de-code@users.noreply.github.com>
2018-04-03 17:11:14 +0100

    added internal grobid network, preventing outbound connections

--- salt/sciencebeam-texture/config/home-deployuser-sciencebeam-texture-smoke_tests.sh
+++ salt/sciencebeam-texture/config/home-deployuser-sciencebeam-texture-smoke_tests.sh
@@ -8,13 +8,9 @@ echo "Texture container"
 smoke_url_ok localhost:4000/
 echo "Sciencebeam container"
 smoke_url_ok localhost:8075/api/
-echo "GROBID container"
-smoke_url_ok localhost:8070/
 echo "Texture nginx"
 smoke_url_ok localhost/
 echo "Sciencebeam nginx"
 smoke_url_ok localhost/api/
-echo "GROBID nginx"
-smoke_url_ok localhost/grobid/
 
 smoke_report
